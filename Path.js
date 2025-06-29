ab90755dc6bb72b55eaf277dac90bb5289c83e1b
Abdul Wahab <wahabnadeem0@gmail.com>
2025-06-30 02:22:41 +0500

    first commit

--- Path.js
+++ Path.js
@@ -1,2 +1,16 @@
+const path = require('path');
 const Path = require('path');
 console.log(__dirname);
+console.log(__filename);
+
+
+const filePath = Path.join("folder","students","data.txt");
+console.log(filePath);
+Path.parse(filePath);
+Path.resolve(filePath);
+Path.basename(filePath);
+path.basename(filePath);
+console.log(Path.basename(filePath));
+console.log(Path.parse(filePath));  
+console.log(parsedPath);    
+const parsedPath = Path.parse(filePath);
