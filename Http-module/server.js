00a4b9b2c762d52609903da95ed9546505566c98
Abdul Wahab <wahabnadeem0@gmail.com>
2025-07-02 02:00:37 +0500

    create first server

---
+++ Http-module/server.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the Home Page');
+    } 
+    else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end("<h1>About Page</h1>");
+    } 
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+    }
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
